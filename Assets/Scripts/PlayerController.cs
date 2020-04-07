714310c86b9c219ffcb7a718ec33a99e33bd2ce0
CrazyCookieBuddy <stockfisch97@gmail.com>
2020-04-07 22:20:12 +0200

    Added Respawn
    Added a respawn function, checking if the player is on ground, and moving the respawn location, and respawning if touching a "danger" tile
    
    Also tweaked the momentum of the dash as well as the speed after dashing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     public float dashStrength;
     public int maxDashes;
     public int dashes;
+    public float afterDashSlowdown = 1.5f;
 
     [HideInInspector] public float dashTime;
     public float maxDashTime;
@@ -30,6 +31,12 @@ public class PlayerController : MonoBehaviour
     public float rayCheckSize;
     Rigidbody2D rb;
     public float dropOffTime = 0.2f;
+
+    [Header("Respawning")]
+    float respawnTimer = 2.0f;
+    float respawnTimerCounter;
+    public GameObject respawnObject;
+    bool canRespawn = true;
 
 
     public LookatDirection LookAtScript;
@@ -58,6 +65,12 @@ public class PlayerController : MonoBehaviour
         currentSpeed = speed;
         rb = GetComponent<Rigidbody2D>();
         StartCoroutine(StartPlay());
+        if (!respawnObject)
+        {
+            respawnObject = new GameObject();
+            respawnObject.transform.position = rb.position;
+            respawnObject.name = "Respawn Object";
+        }
     }
 
     void Update()
@@ -83,7 +96,10 @@ public class PlayerController : MonoBehaviour
                 if (applyingDashSpeed)
                 {
                     dashTimer += Time.deltaTime;
-
+                    if (dashTimer >= maxDashTimer)
+                    {
+                        rb.velocity /= afterDashSlowdown;
+                    }
                     if (dashTimer < maxDashTimer)
                     {
                         if (dashSpeed != Vector2.zero)
@@ -92,10 +108,8 @@ public class PlayerController : MonoBehaviour
                         }
                         else
                         {
-                            Debug.Log("dash speed0");
+                            Debug.Log("dash speed 0");
                         }
-
-
                     }
                     else
                     {
@@ -193,6 +207,7 @@ public class PlayerController : MonoBehaviour
     void Grounded()
     {
 
+
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(rayOrigin.transform.position.x, rayOrigin.transform.position.y), Vector2.down, GetComponentInParent<BoxCollider2D>().bounds.extents.y + rayCheckDistance);
         RaycastHit2D hitLeft = Physics2D.Raycast(new Vector2(rayOrigin.transform.position.x + rayCheckSize, rayOrigin.transform.position.y), Vector2.down, GetComponentInParent<BoxCollider2D>().bounds.extents.y + rayCheckDistance);
         RaycastHit2D hitRight = Physics2D.Raycast(new Vector2(rayOrigin.transform.position.x - rayCheckSize, rayOrigin.transform.position.y), Vector2.down, GetComponentInParent<BoxCollider2D>().bounds.extents.y + rayCheckDistance);
@@ -203,14 +218,40 @@ public class PlayerController : MonoBehaviour
             {
                 StartCoroutine(UnGround());
             }
-
         }
         else
         {
             grounded = true;
             dashes = maxDashes;
 
+           
         }
+        if ((hit.collider != null && hitLeft.collider != null) || (hitLeft.collider != null && hitRight.collider != null) || (hit.collider != null && hitRight.collider != null))  // ((a && b) || (b && c) || (a && c))
+        {
+            respawnTimerCounter += 0.1f;
+            if (respawnTimerCounter >= respawnTimer)
+            {
+                respawnTimerCounter = 0;
+                SetRespawn();
+            }
+        }
+        else
+        {
+            respawnTimerCounter = 0;
+        }
+
+
+    }
+    void SetRespawn(Vector2 position)
+    {
+        respawnObject.transform.position = position;
+    }
+
+    void SetRespawn()
+    {
+        Vector2 position = rb.transform.position;
+        respawnObject.transform.position = position;
+
     }
     void MakeKinematic(bool boo)
     {
@@ -230,7 +271,7 @@ public class PlayerController : MonoBehaviour
 
         if (LookAtScript.aim != Vector3.zero)
         {
-            dashSpeed = Mathf.Clamp(dashTime, 0.5f, maxDashTime) * dashStrength * LookAtScript.aim;
+            dashSpeed = Mathf.Clamp(dashTime, 0.3f, maxDashTime) * dashStrength * LookAtScript.aim;
             applyingDashSpeed = true;
         }
         dashTime = 0;
@@ -266,6 +307,10 @@ public class PlayerController : MonoBehaviour
     {
         //to implement
         Debug.Log("Should die");
+        //temporary:
+
+        rb.transform.position = respawnObject.transform.position;
+        rb.velocity = Vector2.zero;
     }
 
     IEnumerator UnGround()
